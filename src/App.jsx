261b39e5666dbeb271e773166640596efb31c8d3
chohw37 <chohw37@tobetheone.kr>
2025-03-26 11:23:05 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>주식회사 투비더원</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
