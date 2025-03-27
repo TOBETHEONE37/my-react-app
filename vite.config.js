7c753119a3a7a6d280014193717e1042271bd62c
chohw37 <chohw37@tobetheone.kr>
2025-03-27 11:18:00 +0900

    modify

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// tailwind.config.js
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
